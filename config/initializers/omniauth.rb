f76867052d07649dc3c2648e9b6922396ba0aa5e
neeraji2it <neerajonrails@gmail.com>
2013-03-17 18:18:50 +0530

    omniauth changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,12 +5,12 @@
 if Rails.env == 'development' || Rails.env == 'test'
   # development for local host
   Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, '547638578609734', '6e1d9d7592121d86a5b531c15130cf25'
+    provider :facebook, '482351791819327', '95b272b1a22849438af4f8fdfacade44'
   end
 else
   Rails.application.config.middleware.use OmniAuth::Builder do
     # "production" for testing env (http://desolate-everglades-3115.herokuapp.com)
-    provider :facebook, '226062270873991', '091b53e32234b2bd51d54c5f8b1a6344'
+    provider :facebook, '547638578609734', '6e1d9d7592121d86a5b531c15130cf25'
   end
 end
 
