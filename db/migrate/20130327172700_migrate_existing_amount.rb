98801f92b00c95cd48570fe74098795acc819547
neeraji2it <neerajonrails@gmail.com>
2013-03-28 00:10:03 +0530

    fixing ugs

--- db/migrate/20130327172700_migrate_existing_amount.rb
+++ db/migrate/20130327172700_migrate_existing_amount.rb
@@ -2,10 +2,12 @@ class MigrateExistingAmount < ActiveRecord::Migration
   def up
   	Customer.all.each do |customer|
   		total_minits = customer.payments.inject(0){|total, p| total + p.minutes}
-      p 11111111
-      p total_minits
   		used_minutes = customer.time_sheet_entries.each do |time_sheet_entry| 
+            begin
              time_sheet_entry.remining_minits = total_minits = total_minits - ((time_sheet_entry.end_time - time_sheet_entry.start_time)/60)
+            rescue
+              time_sheet_entry.remining_minits = total_minits
+            end
   			time_sheet_entry.save
   		end
   		
