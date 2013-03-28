e044bb74e626cf1497b496f68fa0b7d9148acf04
neeraji2it <neerajonrails@gmail.com>
2013-03-28 08:03:05 +0530

    migration

--- db/migrate/20130327172700_migrate_existing_amount.rb
+++ db/migrate/20130327172700_migrate_existing_amount.rb
@@ -1,7 +1,7 @@
 class MigrateExistingAmount < ActiveRecord::Migration
   def up
   	Customer.all.each do |customer|
-  		total_minits = customer.payments.where(:flovor => [1, 3]).inject(0){|total, p| total + p.minutes}
+  		total_minits = customer.payments.where(:flavor => [1, 3]).inject(0){|total, p| total + p.minutes}
   		used_minutes = customer.time_sheet_entries.each do |time_sheet_entry| 
             begin
              time_sheet_entry.remining_minits = total_minits = total_minits - ((time_sheet_entry.end_time - time_sheet_entry.start_time)/60)
