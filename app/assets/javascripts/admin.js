0965681696c0a66b5da9463d0a795c911c47b05f
neeraji2it <neerajonrails@gmail.com>
2013-04-05 23:29:35 +0530

    resolving conflicts

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -10,8 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//XXX= require jquery
+//= require jquery
 //= require jquery_ujs
-//= require mbl
-//= require notificaitons_management
-//= require script
+//= require twitter/bootstrap
+//= require_tree .
